218bce34b42cfa0944a85c3d7ea26cf4cb1debf2
bjornite <bjornivarteigen@gmail.com>
2016-05-05 14:05:26 +0200

    Added marker tracking and calculating the angle between the cameras z-axis and and the marker coordinate frame

--- main.cpp
+++ main.cpp
@@ -11,12 +11,10 @@ const Mat Distortion_Coefficients =
         (Mat_<double>(5,1) << 0.070528331223347215, 0.26247385180956367, 0, 0, -1.0640942232949715);
 
 
-
 int main() {
 
     //Set up windows:
-    std::string raw_win = "Raw image";
-    cv::namedWindow("raw_win");
+
     std::string undistorted_win = "Undistorted image";
     cv::namedWindow(undistorted_win);
     std::string aruco_win = "Undistorted image with aruco markers";
@@ -30,9 +28,19 @@ int main() {
     vector<int> markerIds;
     vector< vector<Point2f> > markerCorners, rejectedCandidates;
     //cv::aruco::DetectorParameters parameters;
-    const cv::Ptr<cv::aruco::DetectorParameters> parameters = aruco::DetectorParameters::create();
+    //const cv::Ptr<cv::aruco::DetectorParameters> parameters = aruco::DetectorParameters::create();
     //cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
     cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+
+    /*
+    std::string marker_win = "Marker image";
+    cv::namedWindow("marker_win");
+    cv::Mat markerImage;
+
+    cv::aruco::drawMarker(dictionary, 23, 200, markerImage, 1);
+    imwrite("image.jpg",markerImage);
+    cv::imshow(marker_win, markerImage);
+     */
 
     while(true) {
         cv::Mat raw_image;
@@ -43,25 +51,42 @@ int main() {
         cv::undistort(raw_image, imageUndistorted, Camera_Matrix, Distortion_Coefficients);
 
         cv::imshow(undistorted_win, imageUndistorted);
-        cv::imshow(raw_win, raw_image);
 
-
-        cv::aruco::detectMarkers(imageUndistorted, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
+        cv::aruco::detectMarkers(imageUndistorted, dictionary, markerCorners, markerIds);
 
         cv::Mat imageCopy;
         imageUndistorted.copyTo(imageCopy);
 
-        if(markerIds.size() > 0){
-            printf("found marker");
+        if(markerIds.size() > 0) {
+
+            std::vector<Vec3d> rvecs, tvecs;
+
+            cv::aruco::estimatePoseSingleMarkers(markerCorners, 0.1952, Camera_Matrix, Distortion_Coefficients, rvecs,
+                                                 tvecs);
+
+            //cv::aruco::drawDetectedMarkers(imageCopy, markerCorners, markerIds);
+            cv::aruco::drawAxis(imageCopy, Camera_Matrix, Distortion_Coefficients, rvecs, tvecs, 0.5);
+
+            //Code for controlling the robot goes here:
+            //henter x-koordinatet til arucomarkeren i kameraets koordinatsystem
+            double x = tvecs[0][0];
+            //printf("%f\n",x);
+            double z = tvecs[0][2];
+
+            double angle = cvFastArctan(x,z);
+
+            printf("%f\n",angle);
+
+            if(angle > 5 && angle < 180) {
+                printf("Kjør til Høyre\n");
+            } else if (angle < 355) {
+                printf("Kjør til Venstre\n");
+            }
         }
-
-
-        cv::aruco::drawDetectedMarkers(imageCopy, markerCorners, markerIds);
-
         imshow(aruco_win,imageCopy);
 
         int key = cv::waitKey(30);
-        if (key > 0) break;
+        if (key == 'q') break;
     }
 
 }
